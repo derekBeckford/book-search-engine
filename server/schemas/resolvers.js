9b49acfe12e46cc10731d454b1a42235b2f84a44
Derek Beckford <dbeck1207@icloud.com>
2021-10-02 13:31:58 -0500

    changes to routes

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,6 @@
-const { AuthenticationError } = require("apollo-server-express");
-const { User, Book } = require("../models");
+const { User } = require("../models");
 const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require("apollo-server-express");
 
 const resolvers = {
   Query: {
@@ -9,14 +9,24 @@ const resolvers = {
         const userData = await User.findOne({ _id: context.user._id })
           .select("-__v -password")
           .populate("books");
+
         return userData;
       }
 
       throw new AuthenticationError("Not logged in");
     },
   },
-
   Mutation: {
+    addUser: async (parent, args) => {
+      try {
+        const user = await User.create(args);
+
+        const token = signToken(user);
+        return { token, user };
+      } catch (err) {
+        console.log(err);
+      }
+    },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
@@ -33,22 +43,18 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
-
-    addUser: async (parent, args) => {
-      const user = await User.create(args);
-      const token = signToken(user);
-
-      return { token, user };
-    },
-    saveBook: async (parent, { input }, context) => {
+    saveBook: async (parent, args, context) => {
       if (context.user) {
         const updatedUser = await User.findByIdAndUpdate(
           { _id: context.user._id },
-          { $addToSet: { savedBooks: input } },
-          { new: true }
+          // take the input type to replace "body" as the arguement
+          { $addToSet: { savedBooks: args.input } },
+          { new: true, runValidators: true }
         );
+
         return updatedUser;
       }
+
       throw new AuthenticationError("You need to be logged in!");
     },
     removeBook: async (parent, args, context) => {
@@ -58,8 +64,10 @@ const resolvers = {
           { $pull: { savedBooks: { bookId: args.bookId } } },
           { new: true }
         );
+
         return updatedUser;
       }
+
       throw new AuthenticationError("You need to be logged in!");
     },
   },
--- server/server.js
+++ server/server.js
@@ -1,20 +1,27 @@
 const express = require("express");
-const path = require("path");
-const db = require("./config/connection");
-const routes = require("./routes");
+// import ApolloServer
 const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
 
+const path = require("path");
+const db = require("./config/connection");
+
+// const routes = require("./routes");
+
+// import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
 
+// integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
@@ -29,11 +36,15 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-app.use(routes);
+// app.use(routes);
 
 db.once("open", () => {
   app.listen(PORT, () => {
-    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
+});
+db.on("error", (err) => {
+  console.error("MongoDB connection error: ", err);
 });
